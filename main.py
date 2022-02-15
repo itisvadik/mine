c23740c2136e606796ab8c3ad88d82819d4a3a3a
itisvadik <98023026+itisvadik@users.noreply.github.com>
2022-02-15 12:11:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 from key import TOKEN
+from stickers import stickers
 
 
 def main():
@@ -14,9 +15,11 @@ def main():
     echo_handler = MessageHandler(Filters.all, do_echo)
     hello_handler = MessageHandler(Filters.text('Привет, привет'), say_hello)
     keyboard_handler = MessageHandler(Filters.text('Клавиатура, клавиатура'), keyboard)
+    static_handler = MessageHandler(Filters.text('Статистика, статистика'), static)
 
     dispatcher.add_handler(hello_handler)
     dispatcher.add_handler(keyboard_handler)
+    dispatcher.add_handler(static_handler)
     dispatcher.add_handler(echo_handler)
 
     updater.start_polling()
@@ -29,22 +32,22 @@ def do_echo(update: Update, context: CallbackContext) -> None:
     surname = update.message.from_user.last_name
     telegram_id = update.message.chat_id
     username = update.message.from_user.username
-    text = update.message.text if update.message.text else ''
-    sticker = update.message.sticker
+    text = update.message.text if update.message.text else None
+    sticker = update.message.sticker if update.message.sticker else None
     if sticker:
         sticker_id = sticker.file_id
         update.message.reply_sticker(sticker_id)
-        print(sticker, '\n', sticker_id)
+        print(sticker, sticker_id)
     update.message.reply_text(text=f'{text}')
     print(username, ':', text)
 
 
-def say_hello(update: Update, context: CallbackContext) -> None:
+def static(update: Update, context: CallbackContext) -> None:
     name = update.message.from_user.first_name
     surname = update.message.from_user.last_name
     telegram_id = update.message.chat_id
     username = update.message.from_user.username
-    text = update.message.text if update.message.text else ''
+    text = update.message.text
     update.message.reply_text(
         text=f'Привет, {name} {surname}-@{username}\n'
              f'id: {telegram_id}\n'
@@ -73,6 +76,16 @@ def keyboard(update: Update, context: CallbackContext) -> None:
 
         )
     )
+
+
+def say_hello(update: Update, context: CallbackContext) -> None:
+    name = update.message.from_user.first_name
+    surname = update.message.from_user.last_name
+    telegram_id = update.message.chat_id
+    username = update.message.from_user.username
+    text = update.message.text
+    update.message.reply_sticker(stickers["привет"])
+    print(username, ':', text)
 
 
 if __name__ == '__main__':
