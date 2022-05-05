0b9f8ca696b639d2002c5ea9ed2225be773afc39
itisvadik <98023026+itisvadik@users.noreply.github.com>
2022-05-05 14:19:09 +0300

    Delete task 10.py

--- task 10.py
+++
@@ -1,9 +0,0 @@
-number = 16**23 + 4**12 - 32**6
-str_number = ''
-while number > 0:
-    ost = number % 4
-    str_number += str(ost)
-    number //= 4
-print(str_number[::-1])
-for i in range(4):
-    print(str_number.count((str(i))))
